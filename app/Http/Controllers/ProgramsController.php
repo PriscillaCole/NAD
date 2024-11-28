82ee6148c305c9fcc0bc618eab2ac2d35f1b1ee5
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2024-11-28 18:43:29 +0300

    Changes to the create frontend

--- app/Http/Controllers/ProgramsController.php
+++ app/Http/Controllers/ProgramsController.php
@@ -18,11 +18,15 @@ class ProgramsController extends Controller
 
     public function store(Request $request)
     {
+
+        // prnt the request
+        // dd($request->all());
         try {
             // Validate the incoming data
             $validated = $request->validate([
                 'name' => 'required|string|max:255',
                 'description' => 'nullable|string',
+                'user_id' => 'required|exists:admin_users,id',
                 // 'budget' => 'nullable|numeric|min:0', // Validate program budget
                 'outcomes' => 'nullable|array',
                 'outcomes.*.name' => 'required_with:outcomes|string|max:255',
@@ -41,13 +45,17 @@ class ProgramsController extends Controller
                 'outcomes.*.outputs.*.activities.*.budget_lines.*.quantity' => 'required_with:outcomes.*.outputs.*.activities.*.budget_lines|numeric|min:0',
             ]);
 
+     
+
             // Begin transaction
             \DB::beginTransaction();
     
+          
             // Create the program
             $program = Program::create([
                 'name' => $validated['name'],
                 'description' => $validated['description'] ?? null,
+                'user_id' => $validated['user_id'],
                 // 'budget' => $validated['budget'] ?? null,
             ]);
     
